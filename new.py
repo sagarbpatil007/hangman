1d85038a35cc1ec84d444535e8a7563449f08c0a
sagar patil <u1010001@utah.edu>
2017-04-14 15:21:01 -0600

    add hangman information

--- new.py
+++ new.py
@@ -1,1 +1,2 @@
 
+print("If never played hangman then here's little information about Hangman \n Hangman is a paper and pencil guessing game for two or more players. One player thinks of a word, phrase or sentence and the other tries to guess it by suggesting letters or numbers, within a certain number of guesses.")
